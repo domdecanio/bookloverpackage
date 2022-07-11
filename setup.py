7180250a22523720bdab3725a2ad4f69bd5c97b5
Dominick DeCanio <domdecanio@gmail.com>
2022-07-11 16:41:46 -0400

    Completed package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='booklover',
+    version='1.0.0',
+    url='https://github.com/domdecanio/bookloverpackage',
+    author='Dominick DeCanio',
+    description='Booklover functions created in DS 5100 Module 9.',
+    packages=['booklover', 'booklover_test'],    
+    install_requires=[],
+)
